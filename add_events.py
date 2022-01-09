129a3a3cd2e0cd510c7d97e78216999e0da1f20a
Bezbakri <bezbakri@gmail.com>
2022-01-09 16:55:32 +0530

    Added event name field

--- add_events.py
+++ add_events.py
@@ -19,7 +19,8 @@ import sqlite3
 
 
 #the input variable name
-event_to_add = ""
+event_to_add_date = ""
+event_to_add_name = ""
 
 
 
@@ -28,9 +29,10 @@ event_to_add = ""
 
 def getValue():
     global event_to_add
-    event_to_add = userInput.get()
-    
-    event_var.set("")
+    event_to_add_date = userInput_date.get()
+    event_to_add_name = userInput_name.get()
+    event_var_date.set("")
+    event_var_name.set("")
 
 
 
@@ -39,13 +41,17 @@ def add_event():
     addition.title("Add events")
     addition.configure(background = "#B1F5F5")
     addition.geometry("800x800")
-    event_var = StringVar()
+    event_var_date = StringVar()
+    event_var_name = StringVar()
     header = Label(addition, text = "Add an event. Type it in YYYY/MM/DD HH:mm:SS Format", font = "consolas 11")
     header.grid(row = 2, column = 1, padx = 20)
     
-    userInput = Entry(addition, textvariable = event_var).grid(row = 4, column = 1)
-    
-    buttonAddEvent = Button(addition, text = "Add event", command = "call_result").grid(row = 6, column = 1)
+    userInput_date = Entry(addition, textvariable = event_var_date).grid(row = 4, column = 1)
+    header = Label(addition, text = "Type in the event name", font = "consolas 11")
+    header.grid(row = 6, column = 1, padx = 20)
+    userInput_name = Entry(addition, textvariable = event_var_name).grid(row = 7, column = 1)
+
+    buttonAddEvent = Button(addition, text = "Add event", command = "call_result").grid(row = 9, column = 1)
     
     
     
